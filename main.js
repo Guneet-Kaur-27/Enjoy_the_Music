6c6946756316232e814dfeb1cdea0e8b8b9717d1
Guneet-Kaur-27 <76588587+Guneet-Kaur-27@users.noreply.github.com>
2021-06-01 12:01:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function play_1() {
+
+    document.getElementById("audio_1").play();
+}
+
+function play_2() {
+
+    document.getElementById("audio_2").play();
+}
+
+function play_3() {
+
+    document.getElementById("audio_3").play();
+}
+
+function play_4() {
+
+    document.getElementById("audio_4").play();
+}
+
